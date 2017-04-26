8730349c230e8a351d8bdc548e6d43b4a4222817
dzz <79570218@qq.com>
2017-04-26 12:20:30 +0800

    init

---
+++ config/application.rb
@@ -0,0 +1,22 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module MasterRailsByMall
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    #希望手工控制，令generator命令不自动生成参数文件
+    config.generators do |generator|
+      generator.assets false #generator生成控制器的时候，不生成js和css
+      generator.test_framework false #generator生成model的时候，不生成test
+    end
+
+  end
+end
